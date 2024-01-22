a54184ecdcf57c16e2a6dc986dd1b55ce5e9eafa
Documenter.jl <documenter@juliadocs.github.io>
2024-01-22 13:32:42 +0000

    build based on 3e3b3b7

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = DataProcessforDQMC","category":"page"},{"location":"#DataProcessforDQMC","page":"Home","title":"DataProcessforDQMC","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for DataProcessforDQMC.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [DataProcessforDQMC]","category":"page"}]
+}
