1ae4e22fb6220034f1d7f1a6802ae86929991bf0
wangfh5 <wangfohong@126.com>
2024-01-22 22:00:57 +0800

    hello

--- src/DataProcessforDQMC.jl
+++ src/DataProcessforDQMC.jl
@@ -1,5 +1,14 @@
 module DataProcessforDQMC
 
+using DelimitedFiles
+using Statistics
+using Printf
+using LaTeXStrings
+
+println("Welcome to DataProcessforDQMC!")
+
 # Write your package code here.
+myfunction1(x) = x + 1
+myfunction2(x) = x * 2
 
 end
